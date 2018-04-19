9afcb33aeaeb6cd0cfc77c2f7c47b7090572f89c
Dumie1 <37332966+Dumie1@users.noreply.github.com>
2018-04-19 19:02:45 +0200

    Create step3.js

---
+++ week1/step3.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let age;                           
+console.log("My age is :" + age);  
+console.log(age);                  
+age = 20;                          
+console.log("My age is :" + age);  
+console.log(age); 
