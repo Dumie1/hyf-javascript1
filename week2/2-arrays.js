932077ad8f1a14416ccefcb606ddec4c5d98b7eb
Dumie1 <dumiedumara@gmail.com>
2018-05-12 23:15:12 +0200

    added some homework corrections on file 2 and 4

--- week2/2-arrays.js
+++ week2/2-arrays.js
@@ -1,6 +1,34 @@
 "use strict";
 
 let favoriteAnimals = ["blowfish", "capricorn", "giraffe"];
+const maurosFavoriteAnimal = "turtle";
+favoriteAnimals.push(maurosFavoriteAnimal);
+console.log(favoriteAnimals);
+
+const jimsFavoriteAnimal = "meerkat";
+favoriteAnimals.splice(1, 0, jimsFavoriteAnimal); 
+console.log(
+    "I thing the new value of favoriteAnimals is: blowfish, meerkat, capricorn, giraffe, turtle."
+);
+console.log(favoriteAnimals);
+
+console.log("The array has a length of: " + favoriteAnimals.length);
+
+favoriteAnimals.splice(3, 1); // no giraffe
+console.log(favoriteAnimals);
+
+const indexOfMeerkat = favoriteAnimals.indexOf("meerkat");
+favoriteAnimals.splice(indexOfMeerkat, 1);
+console.log(favoriteAnimals);
+console.log("The item you are looking for is at index: " + indexOfMeerkat);
+
+
+
+//My original code below before corrections.
+
+/*"use strict";
+
+let favoriteAnimals = ["blowfish", "capricorn", "giraffe"];
 console.log(favoriteAnimals);
 
 favoriteAnimals.push("turtle");
@@ -18,8 +46,12 @@ console.log(favoriteAnimals);
 console.log("Item to be deleted is at index :" + favoriteAnimals.indexOf("meerkat"));
 favoriteAnimals.splice(1, 1);
 console.log(favoriteAnimals);
-
+*/
 
 /*References - Using splice() 
 https://developer.mozilla.org/nl/docs/Web/JavaScript/Reference/Global_Objects/Array/splice 
-https://www.lynda.com/JavaScript-tutorials/Properties-methods-arrays/574716/612037-4.html?autoplay=true*/
+https://www.lynda.com/JavaScript-tutorials/Properties-methods-arrays/574716/612037-4.html?autoplay=true
+w3schools https://www.w3schools.com/js/js_array_methods.asp
+youtube https://www.youtube.com/watch?v=H4Sf4suR3C4
+http://www.hostingadvice.com/how-to/javascript-remove-element-array/
+*/
--- week2/4-maartje.js
+++ week2/4-maartje.js
@@ -1,3 +1,64 @@
+"use strict";
+const monday = [
+  {
+    name: "Write a summary HTML/CSS",
+    duration: 180
+  },
+  {
+    name: "Some web development",
+    duration: 120
+  },
+  {
+    name: "Fix homework for class10",
+    duration: 20
+  },
+  {
+    name: "Talk to a lot of people",
+    duration: 200
+  }
+];
+
+const tuesday = [
+  {
+    name: "Keep writing summary",
+    duration: 240
+  },
+  {
+    name: "Some more web development",
+    duration: 180
+  },
+  {
+    name: "Staring out the window",
+    duration: 10
+  },
+  {
+    name: "Talk to a lot of people",
+    duration: 200
+  },
+  {
+    name: "Look at application assignments new students",
+    duration: 40
+  }
+];
+
+const tasks = monday.concat(tuesday);
+let hourSalary = 23;
+let hoursWorked = 0;
+const earnings = hoursWorked * hourSalary;
+
+for (let i = 0; i < tasks.length; i++) {
+  if (tasks[i].duration >= 120) {
+    hoursWorked += tasks[i].duration / 60;
+  }
+}
+
+console.log("Maartje has earned: " + earnings.toFixed(2) + " euros.");
+
+
+
+
+//my previous code below
+/*
 'use strict';
 
 const monday = [
@@ -57,3 +118,5 @@ console.log("Hours paid= ", hoursWorked, "hour");
 console.log(Math.floor(hoursWorked));
 console.log("Maatrje`s wages will be hourWages multiplied by  workingTime per hour");
 console.log("Maatrje has earned €:", earnings.toFixed(0));
+
+*/
